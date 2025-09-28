c15c97667728ad4fc197420917b421c1729f3dc0
Sarper Yasin MUTLUBAY <yasinmutlubay@hotmail.com.tr>
2025-09-28 19:52:34 +0300

    Off-story refactor: set_version_data updated

--- src/scripts/set_version_data.sh
+++ src/scripts/set_version_data.sh
@@ -7,14 +7,12 @@ echo "PIPELINE_ID: $PIPELINE_ID"
 echo "CIRCLE_BRANCH: $CIRCLE_BRANCH"
 
 {
-    echo "export VERSION_PREFIX=${BASE_VERSION}.${PIPELINE_ID}"
-    echo "export VERSION_SUFFIX=\$CIRCLE_BRANCH"
-    echo "if [ \"$CIRCLE_BRANCH\" == \"main\" ]; then export VERSION_SUFFIX=test; fi"
-    echo "if [ \"$CIRCLE_BRANCH\" != \"main\" ]; then export NUGET_VERSION_SUFFIX=\$CIRCLE_BRANCH; fi"
-    echo "export FULL_VERSION=\$VERSION_PREFIX-\$VERSION_SUFFIX"
+    echo "export VERSION_PREFIX=$BASE_VERSION.$PIPELINE_ID;"
+    echo "export VERSION_SUFFIX=$CIRCLE_BRANCH;"
+    echo "if [ $CIRCLE_BRANCH == \"main\" ]; then export VERSION_SUFFIX=test; fi"
+    echo "if [ $CIRCLE_BRANCH != \"main\" ]; then export NUGET_VERSION_SUFFIX=$CIRCLE_BRANCH; fi"
+    echo "export FULL_VERSION=$VERSION_PREFIX-$VERSION_SUFFIX"
 } >> "$BASH_ENV"
-
-source "$BASH_ENV"
 
 echo "Version variables Set"
 echo "VERSION_PREFIX: $VERSION_PREFIX"
