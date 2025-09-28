b01379d946ff026d614457da95d9f5aed25ef19d
Sarper Yasin MUTLUBAY <yasinmutlubay@hotmail.com.tr>
2025-09-29 00:47:50 +0300

    Off-story refactor: add_nuget_source.sh file listing feature added

--- src/scripts/dotnet/add_nuget_source.sh
+++ src/scripts/dotnet/add_nuget_source.sh
@@ -6,3 +6,6 @@ NUGET_NAME=$(circleci env subst "${NUGET_NAME}")
 
 echo "Adding NuGet source \"$NUGET_NAME\" with URL \"$NUGET_URL\"..."
 dotnet nuget add source --username "$NUGET_USERNAME" --password "$NUGET_PASSWORD" --store-password-in-clear-text --name "$NUGET_NAME" "$NUGET_URL"
+
+echo "Listing all NuGet sources..."
+dotnet nuget list source
