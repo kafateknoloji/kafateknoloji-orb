92f7f9a30926263aa7524f44119f142666ed532c
Sarper Yasin MUTLUBAY <yasinmutlubay@hotmail.com.tr>
2025-09-28 16:59:27 +0300

    off-story fix: set_version_data script fixed

--- src/scripts/set_version_data.sh
+++ src/scripts/set_version_data.sh
@@ -6,8 +6,8 @@ PIPELINE_ID=$(circleci env subst "${PIPELINE_ID}")
     echo 
     "export VERSION_PREFIX=$BASE_VERSION.$PIPELINE_ID"
     "export VERSION_SUFFIX=$CIRCLE_BRANCH;"
-    "if [ $CIRCLE_BRANCH == "main" ]; then export VERSION_SUFFIX=test; fi"
-    "if [ $CIRCLE_BRANCH != "main" ]; then export NUGET_VERSION_SUFFIX=$CIRCLE_BRANCH; fi"
+    "if [ $CIRCLE_BRANCH == \"main\" ]; then export VERSION_SUFFIX=test; fi"
+    "if [ $CIRCLE_BRANCH != \"main\" ]; then export NUGET_VERSION_SUFFIX=$CIRCLE_BRANCH; fi"
     "export FULL_VERSION=$VERSION_PREFIX-$VERSION_SUFFIX;"
 } >> "$BASH_ENV"
 
