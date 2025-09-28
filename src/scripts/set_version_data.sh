a3e21dcb66d3f099ec679a82098f0828a58a5636
Sarper Yasin MUTLUBAY <yasinmutlubay@hotmail.com.tr>
2025-09-28 19:21:35 +0300

    Off-story refactor: Small changes

--- src/scripts/set_version_data.sh
+++ src/scripts/set_version_data.sh
@@ -3,12 +3,11 @@ BASE_VERSION=$(circleci env subst "${BASE_VERSION}")
 PIPELINE_ID=$(circleci env subst "${PIPELINE_ID}")
 
 {
-    echo 
-    "export VERSION_PREFIX=$BASE_VERSION.$PIPELINE_ID"
-    "export VERSION_SUFFIX=$CIRCLE_BRANCH;"
-    "if [ $CIRCLE_BRANCH == \"main\" ]; then export VERSION_SUFFIX=test; fi"
-    "if [ $CIRCLE_BRANCH != \"main\" ]; then export NUGET_VERSION_SUFFIX=$CIRCLE_BRANCH; fi"
-    "export FULL_VERSION=$VERSION_PREFIX-$VERSION_SUFFIX;"
+    echo "export VERSION_PREFIX=$BASE_VERSION.$PIPELINE_ID"
+    echo "export VERSION_SUFFIX=$CIRCLE_BRANCH"
+    echo "if [ \"$CIRCLE_BRANCH\" == \"main\" ]; then export VERSION_SUFFIX=test; fi"
+    echo "if [ \"$CIRCLE_BRANCH\" != \"main\" ]; then export NUGET_VERSION_SUFFIX=$CIRCLE_BRANCH; fi"
+    echo "export FULL_VERSION=\$VERSION_PREFIX-\$VERSION_SUFFIX"
 } >> "$BASH_ENV"
 
 echo "Version variables Set"
