dfbd6ddb7bda7b7bc3bb8cbdbe59049ac475ea89
Sarper Yasin MUTLUBAY <yasinmutlubay@hotmail.com.tr>
2025-09-29 00:04:29 +0300

    Off-story refactor: Nuget command improved

--- src/scripts/dotnet/add_nuget_source.sh
+++ src/scripts/dotnet/add_nuget_source.sh
@@ -4,4 +4,5 @@ NUGET_PASSWORD=$(circleci env subst "${NUGET_PASSWORD}")
 NUGET_URL=$(circleci env subst "${NUGET_URL}")
 NUGET_NAME=$(circleci env subst "${NUGET_NAME}")
 
+echo "Adding NuGet source \"$NUGET_NAME\" with URL \"$NUGET_URL\"..."
 dotnet nuget add source --username "$NUGET_USERNAME" --password "$NUGET_PASSWORD" --store-password-in-clear-text --name "$NUGET_NAME" "$NUGET_URL"
--- src/scripts/dotnet/build.sh
+++ src/scripts/dotnet/build.sh
@@ -2,5 +2,5 @@
 PROJECT_PATH=$(circleci env subst "${PROJECT_PATH}")
 ENVIRONMENT_NAME=$(circleci env subst "${ENVIRONMENT_NAME}")
 
-echo "Building $PROJECT_PATH project for $ENVIRONMENT_NAME environment..."
+echo "Building \"$PROJECT_PATH\" project for \"$ENVIRONMENT_NAME\" environment..."
 dotnet build "$PROJECT_PATH" -c Release -p:EnvironmentName="$ENVIRONMENT_NAME" --no-restore
