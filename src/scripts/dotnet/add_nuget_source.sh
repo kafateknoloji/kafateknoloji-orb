9ed546a43187f286fce24ead793996ce60918f5b
Sarper Yasin MUTLUBAY <yasinmutlubay@hotmail.com.tr>
2025-09-29 20:47:47 +0300

    Off-story refactor: Nuget add source improvements.

--- src/scripts/dotnet/add_nuget_source.sh
+++ src/scripts/dotnet/add_nuget_source.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-NUGET_NAME=$(circleci env subst "${NUGET_NAME}")
-NUGET_URL_TEST=$(circleci env subst "${NUGET_URL_TEST}")
+NUGET_SOURCE_URL=$(circleci env subst "${NUGET_SOURCE_URL}")
+NUGET_SOURCE_USERNAME=$(circleci env subst "${NUGET_SOURCE_USERNAME}")
+NUGET_SOURCE_PASSWORD=$(circleci env subst "${NUGET_SOURCE_PASSWORD}")
+NUGET_SOURCE_NAME=$(circleci env subst "${NUGET_SOURCE_NAME}")
 
 cat <<EOF > ./nuget_config.txt
 {
   "packageSources": {
-    "$NUGET_NAME": "$NUGET_URL_TEST"
+    "NUGET_SOURCE_URL": "$NUGET_SOURCE_URL",
+    "NUGET_SOURCE_NAME": "$NUGET_SOURCE_NAME",
+    "NUGET_SOURCE_USERNAME": "$NUGET_SOURCE_USERNAME",
+    "NUGET_SOURCE_PASSWORD": "$NUGET_SOURCE_PASSWORD"
   }
 }
 EOF
@@ -15,8 +20,8 @@ echo "--------------------------------"
 cat ./nuget_config.txt
 
 echo "--------------------------------"
-echo "Adding NuGet source \"$NUGET_NAME\" with URL \"$VAR_NUGET_URL\"..."
-dotnet nuget add source --username "$VAR_NUGET_USERNAME" --password "$VAR_NUGET_PASSWORD" --store-password-in-clear-text --name "$NUGET_NAME" "$VAR_NUGET_URL"
+echo "Adding NuGet source \"$NUGET_SOURCE_NAME\" with URL \"$NUGET_SOURCE_URL\"..."
+dotnet nuget add source --username "$NUGET_SOURCE_USERNAME" --password "$NUGET_SOURCE_PASSWORD" --store-password-in-clear-text --name "$NUGET_SOURCE_NAME" "$NUGET_SOURCE_URL"
 
 echo "Listing all NuGet sources..."
 dotnet nuget list source
